20f722a095e1ecf05c76d3b583b0f593e87c994d
Assaf Shomer <assafshomer@gmail.com>
2013-01-16 08:46:12 -0800

    initial commit, with Gemfile and gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FightSpam::Application.config.secret_token = '87f39aee97e5c662a59e871e1fbc4a56a5982f3ac42d0b11b56587468a782f4d5485c1df369b82003af0716e72880f10e187c6da53180dccc0d0585cf9c6004e'
